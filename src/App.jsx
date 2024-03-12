4c3b37230c7b75831b43b64a5e92ca7bbd32893a
Edwin <epinchao@krugerschool.edu.ec>
2024-03-11 20:12:48 -0500

    Material UI  alert y tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Login from "./pages/Login";
 import Home from "./pages/Home";
-
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Home />} />
       </Routes>
     </>
   );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,52 +1,81 @@
+import { useRef, useState } from "react";
 import { Box, Button, TextField } from "@mui/material";
 import { collection, query, where, getDocs } from "firebase/firestore";
-import { useRef } from "react";
+import Alert from "@mui/material/Alert";
+import AlertTitle from "@mui/material/AlertTitle";
 import { db } from "../firebase";
 
 const Login = () => {
+  const [alertMessage, setAlertMessage] = useState("");
+  const [alertSeverity, setAlertSeverity] = useState("success");
+  const [showAlert, setShowAlert] = useState(false);
   const email = useRef("");
   const password = useRef("");
   const usersRef = collection(db, "users");
+
+  const handleAlertClose = () => {
+    setShowAlert(false);
+  };
+
   const login = async (e) => {
     e.preventDefault();
-    /**Realizar la conexion del email y constrasena son las correctas */
     const search = query(usersRef, where("email", "==", email.current.value));
     const result = await getDocs(search);
 
-    console.log(result);
     if (result.docs.length > 0) {
       const user = result.docs[0].data();
       if (user.password === password.current.value) {
-        console.log("Login Sucess");
-        console.log("Redirige");
+        setAlertSeverity("success");
+        setAlertMessage("Login Success");
+        setShowAlert(true);
+        console.log("Redirect");
       } else {
-        console.log("Incorrect");
+        setAlertSeverity("error");
+        setAlertMessage("Incorrect Password");
+        setShowAlert(true);
       }
+    } else {
+      setAlertSeverity("error");
+      setAlertMessage("User Not Found");
+      setShowAlert(true);
     }
   };
+
   return (
     <>
-      <h1>Acceso</h1>
-      <Box component="form" sx={{ p: 2, border: "1px dashed grey" }}>
+      <h1 className="text-3xl font-semibold text-center mb-4">Acceso</h1>
+      <Box
+        component="form"
+        sx={{ p: 2, border: "1px dashed grey" }}
+        className="max-w-md mx-auto"
+      >
         <TextField
-          className="w-full my-4"
+          className="w-full mb-4"
           label="Email"
           type="email"
           variant="outlined"
           inputRef={email}
         />
-        <br />
         <TextField
-          className="w-full my-4"
-          label="password"
+          className="w-full mb-4"
+          label="Password"
           type="password"
           variant="outlined"
           inputRef={password}
         />
-        <br />
-        <Button type="submit" onClick={login} variant="contained">
+        <Button onClick={login} variant="contained" className="w-full">
           Login
         </Button>
+      </Box>
+      <Box className="max-w-md mx-auto mt-4">
+        {showAlert && (
+          <Alert severity={alertSeverity} onClose={handleAlertClose}>
+            <AlertTitle>
+              {alertSeverity === "success" ? "Success" : "Error"}
+            </AlertTitle>
+            {alertMessage}
+          </Alert>
+        )}
       </Box>
     </>
   );
